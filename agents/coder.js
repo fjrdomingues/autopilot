34370d4189c46cc3660ed6c422e348f88be97156
Ofir Petrushka <hatkyinc@gmail.com>
2023-04-08 14:30:30 +1000

    Coder output = path file

--- agents/coder.js
+++ agents/coder.js
@@ -5,8 +5,17 @@ async function suggestChanges(task, sourceCode) {
     const prompt = 
 ` 
 USER INPUT: ${task}
-YOUR TASK: You are a senior software developer. Do what is asked in the USER INPUT
-RESPONSE FORMAT: reply with text and provide the necessary assets to accomplish the USER INPUT
+YOUR TASK: You are a senior software developer. Do what is asked in the USER INPUT.
+RESPONSE FORMAT: A valid patch format with a code update based on the USER INPUT. Example format:
+"
+diff --git a/[file path] b/[file path]
+index [old file hash]..[new file hash] [mode]
+--- a/[file path]
++++ b/[file path]
+@@ -[old line number],[old line number] +[new line number],[new line number] @@
+ [changed lines]
+"
+Code only. Do not add explanations or comments.
 SOURCE CODE - This is code from the existing codebase of this project:
 \`\`\`
 ${sourceCode}
@@ -14,7 +23,6 @@ ${sourceCode}
 `
   
     const reply = await callGPT(prompt, process.env.ADVANCED_MODEL);
-  
     return reply;
 }
 
--- ui.js
+++ ui.js
@@ -40,16 +40,13 @@ async function readAllSummaries() {
   return summaries;
 }
 
-// Saves output to .md file
-function saveOutput(task, solution) {
- // Save the solution to a file in the "suggestions" folder
- const suggestionsDir = path.join(__dirname, 'suggestions');
- const fileName = `${Date.now()}.md`;
+function savePatchFile(patch) {
+  const suggestionsDir = path.join(__dirname, 'suggestions');
+  const fileName = `${Date.now()}.patch`;
+  const filePath = path.join(suggestionsDir, fileName)
 
- // Write the suggestion to the file
- const filePath = path.join(suggestionsDir, fileName)
- fs.writeFileSync(filePath, `# TASK \n ${task}\n# SOLUTION\n\`\`\`json\n${solution}\`\`\``);
- return filePath
+  fs.writeFileSync(filePath, patch);
+  return filePath
 }
 
 // Asks user for a task
@@ -99,9 +96,9 @@ async function main(task) {
 
   //Sends the saved output to GPT and ask for the necessary changes to do the TASK
   const solution = await agents.coder(task, tempOutput);
-  const solutionPath = saveOutput(task, solution);
-  
-  console.log(chalk.green("Solution Ready:", solutionPath));
+  console.log("Solution:", solution);
+  const solutionPath = savePatchFile(solution);
+
   return solution
 }
 
