b9c1ef7f6aef012e9e2bd10cde6eb0494edddf49
Ofir Petrushka <hatkyinc@gmail.com>
2023-04-09 18:00:42 +1000

    refactor readAllSummaries

--- ui.js
+++ ui.js
@@ -7,38 +7,10 @@ const chalk = require('chalk');
 const path = require('path');
 const ignorePattern = ['node_modules/**/*'];
 const { getTaskInput } = require('./modules/userInputs');
+const { readAllSummaries } = require('./modules/summaries');
 
 // Agents
 const agents = require('./agents');
-
-// Gets all .ai.txt files (summaries)
-async function readAllSummaries() {
-  var files = [];
-  try {
-    console.log("Getting Summary");
-    files = await fg(path.posix.join(process.env.CODE_DIR, '**/*.ai.txt'), { ignore: ignorePattern });
-  } catch (err) {
-    console.error("Error in fast-glob:", err);
-    throw err;
-  }
-
-  if (files.length === 0) {
-    console.log("No matching files found. Try running `node createSummaryOfFiles` first.");
-    throw new Error("Can not run without Summaries. Try running `node createSummaryOfFiles` first.");
-  }
-
-  let summaries = "";
-  console.log("Files found:", files);
-  for (const file of files) {
-    try {
-      const summary = fs.readFileSync(file, 'utf-8');
-      summaries += summary + '\n\n';
-    } catch (error) {
-      console.error("Error reading file:", file, error);
-    }
-  }
-  return summaries;
-}
 
 // Saves output to .md file
 function saveOutput(task, solution) {
