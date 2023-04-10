66c8d963221021fea7371b6c390546e95181f072
Ofir Petrushka <hatkyinc@gmail.com>
2023-04-10 14:04:45 +1000

    refactor countTokens

---
+++ modules/tokenHelper.js
@@ -0,0 +1,16 @@
+const { get_encoding } = require('@dqbd/tiktoken');
+
+/**
+* Counts the number of tokens in the input string.
+* @param {string} input - The input string to tokenize.
+* @returns {number} - The number of tokens in the input string.
+*/
+function countTokens(input) {
+    const encoder = get_encoding("cl100k_base")
+    const tokens = encoder.encode(input);
+    const tokenCount = tokens.length;
+    encoder.free();
+    return tokenCount;
+}
+
+module.exports = { countTokens };
