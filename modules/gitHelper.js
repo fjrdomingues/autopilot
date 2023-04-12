f2532e117182afffd897cf15758dfabb34701401
Ofir Petrushka <hatkyinc@gmail.com>
2023-04-13 08:59:41 +1000

    refactor dir usage; add dir to gitDiff

--- modules/gitHelper.js
+++ modules/gitHelper.js
@@ -3,8 +3,8 @@ const { exec } = require('child_process');
 /**
  * Prints the output of git diff to the console.
  */
-function printGitDiff(){
-    exec('git diff', (error, stdout, stderr) => {
+function printGitDiff(dir){
+    exec(`cd ${dir}; git diff`, (error, stdout, stderr) => {
         if (error) {
           console.error(`Error executing git diff: ${error}`);
           return;
