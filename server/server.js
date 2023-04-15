d6a4c4f1718549fabbfe0678631e35a665e20f1d
Fabio Domingues <fjrdomingues@gmail.com>
2023-04-15 18:08:32 +0100

    removed git stuff

--- server/server.js
+++ server/server.js
@@ -103,25 +103,6 @@ app.post('/issue', async (req, res) => {
 });
 
 async function initApp() {
-  const password = process.env.GIT_PASSWORD
-  // init git repo
-  execSync('git init')
-  execSync(`git remote add origin https://fjrdomingues:${process.env.GH_TOKEN}@github.com/fjrdomingues/autopilot.git`)
-  
-
-
-  // // save heroku folder
-  // execSync('mv .heroku /tmp')
-  // Fetch the latest code from the remote repository
-  execSync('git fetch origin');
-  // // Reset the working directory to the latest commit
-  // execSync('git reset --hard FETCH_HEAD');
-  // // get heroku folder
-  // execSync('mv /tmp/.heroku .');
-
-  execSync('git config --global user.email "fjrdomingues@gmail.com"');
-  execSync('git config --global user.name "Fabio Domingues"');
-
   // creates summaries every time that the app is deployed
   await createSummaries(true, true, false)
   // execSync('node createSummaryOfFiles --all --auto')
