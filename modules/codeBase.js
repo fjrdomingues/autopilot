69083970bf36b8ee026c6e61880f7481511fcbf3
Ofir Petrushka <hatkyinc@gmail.com>
2023-04-17 08:58:33 +1000

    comments

--- modules/codeBase.js
+++ modules/codeBase.js
@@ -21,26 +21,41 @@ async function codeBaseFullIndex(codeBaseDirectory, model){
 };
 
 
-// Calculate and display the project size and cost estimation
+/**
+ * 
+ * Calculates and displays the estimated size and cost of a project based on the number of tokens in a given directory path.
+ * @param {string} directoryPath - The path to the directory containing the project files.
+ * @param {object} model - The cost model to use for calculating the cost.
+ */
 function printCostEstimation(directoryPath, model){
-    const getDirectoryTokensCount = require('./modules/directoryHelper');
-    tokenCount = getDirectoryTokensCount(directoryPath)
-    cost = calculateTokensCost(model, tokenCount, null, tokenCount)
-    
-    console.log(`Project size: ~${tokenCount} tokens, estimated cost: $${chalk.yellow(cost.toFixed(4))}`);
-  }
+  const getDirectoryTokensCount = require('./modules/directoryHelper');
+  tokenCount = getDirectoryTokensCount(directoryPath)
+  cost = calculateTokensCost(model, tokenCount, null, tokenCount)
   
-  async function approveIndexing(){
-    const prompts = require('prompts');
-  
-    const proceed = await prompts({
-      type: 'confirm',
-      name: 'value',
-      message: 'Proceed with summarizing the project?',
-    });
-    return proceed.value;
-  }
-  
+  console.log(`Project size: ~${tokenCount} tokens, estimated cost: $${chalk.yellow(cost.toFixed(4))}`);
+}
+
+/**
+ * Asks the user for approval to proceed with summarizing the project.
+ * @returns {boolean} - Whether the user has approved the indexing.
+ */
+async function approveIndexing(){
+  const prompts = require('prompts');
+
+  const proceed = await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: 'Proceed with summarizing the project?',
+  });
+  return proceed.value;
+}
+
+/**
+ * Indexes the full codebase interactively.
+ * @param {string} codeBaseDirectory - The directory of the codebase to be indexed.
+ * @param {object} model - The model used for indexing.
+ * @returns {Promise<void>}
+ */
 async function codeBaseFullIndexInteractive(codeBaseDirectory, model){
     printCostEstimation(codeBaseDirectory, model);
 
