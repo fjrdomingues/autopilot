959484a30843bee1ab31f462d5c6855388ffae7a
Ofir Petrushka <hatkyinc@gmail.com>
2023-04-22 08:26:08 +1000

    Refactor reindex interactive decision

--- ui.js
+++ ui.js
@@ -114,6 +114,24 @@ function getOptions(task, test){
 
 
 /**
+ * Asynchronously reindexes the codebase located at the specified directory, using the specified model for indexing.
+ * @param {string} codeBaseDirectory - The path to the codebase directory.
+ * @param {Object} model - The model used for indexing the codebase.
+ * @param {boolean} interactive - A flag indicating whether to use interactive indexing or not.
+ * @returns {Promise} A promise that resolves when the indexing process is complete.
+ */
+async function reindexCodeBase(codeBaseDirectory, model, interactive) {
+  if (interactive) {
+    const { codeBaseFullIndexInteractive } = require('./modules/codeBase');
+    await codeBaseFullIndexInteractive(codeBaseDirectory, model);
+  } else {
+    const { codeBaseFullIndex } = require('./modules/codeBase');
+    await codeBaseFullIndex(codeBaseDirectory, model);
+  }
+}
+
+
+/**
  * 
  * @param {string} task
  * @returns {string}
@@ -159,13 +177,7 @@ async function main(task, test=false) {
     await initCodeBase(codeBaseDirectory, interactive);
   } else {
     if (reindex){
-      if (interactive){
-        const { codeBaseFullIndexInteractive } = require('./modules/codeBase');
-        await codeBaseFullIndexInteractive(codeBaseDirectory, process.env.INDEXER_MODEL);
-      } else {
-        const { codeBaseFullIndex } = require('./modules/codeBase');
-        await codeBaseFullIndex(codeBaseDirectory);
-      }
+      await reindexCodeBase(codeBaseDirectory, process.env.INDEXER_MODEL, interactive);
     } else if (indexGapFill){
       const { codeBaseGapFill } = require('./modules/codeBase');
       const ret = await codeBaseGapFill(codeBaseDirectory);
@@ -249,3 +261,4 @@ if (require.main === module) main();
 
 
 module.exports = { main }
+
