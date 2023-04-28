9ac443f27b6c24013a13d9aa34d6eb4bba96164e
Ofir Petrushka <hatkyinc@gmail.com>
2023-04-29 09:51:32 +1000

    cleanup taskComplexity

--- agents/index.js
+++ agents/index.js
@@ -1,11 +1,9 @@
-const taskComplexity = require('./taskComplexity');
 const getFiles = require('./getFiles');
 const coder = require('./coder');
 const reviewer = require('./reviewer');
 const advisor = require('./advisor');
 
 module.exports = {
-    taskComplexity,
     getFiles,
     coder,
     reviewer,
